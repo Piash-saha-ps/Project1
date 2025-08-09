659e85779e3bc393f0ccc36c8833eb818da90ead
Piash Saha <Piash.saha50@gmail.com>
2025-08-09 16:27:52 +0600

    Added

--- pseller/act.sql
+++ pseller/act.sql
@@ -4,3 +4,9 @@ CREATE TABLE activity_log (
     description TEXT NOT NULL,
     timestamp DATETIME
 );
+
+CREATE TABLE customers (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
