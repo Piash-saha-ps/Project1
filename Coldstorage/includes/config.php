5bdcdfe4734edde1d9f2d47172cbea627289c970
Piash Saha <Piash.saha50@gmail.com>
2025-08-21 17:53:16 +0600

    Added

--- Coldstorage/includes/config.php
+++ Coldstorage/includes/config.php
@@ -3,7 +3,7 @@
 $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
-$db_name = 'project';
+$db_name = 'inventory';
 
 // Create connection
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
--- Coldstorage/includes/dbConnection.php
+++ Coldstorage/includes/dbConnection.php
@@ -3,7 +3,7 @@
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
-$dbname = "project"; 
+$dbname = "inventory"; 
 
 
 // Create connection
